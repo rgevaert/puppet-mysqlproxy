5ce02df45143e3603b2281da56514fd4e8cbb285
Rudy Gevaert <rudy.gevaert@ugent.be>
2011-09-01 15:30:22 +0200

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: mysqlproxy
+#
+# This module manages mysqlproxy
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class mysqlproxy {
+
+
+}
