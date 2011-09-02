f7cb9190a2f7f6654b076434d024ac2718c87f96
Rudy Gevaert <rudy.gevaert+github@ugent.be>
2011-09-02 11:29:03 +0200

    Check if lua script exists.

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,4 +4,29 @@ class mysqlproxy::install
     'mysql-proxy':
       ensure => installed;
   }
+  file {
+    "/etc/mysql-proxy":
+      owner  => root,
+      group  => root,
+      mode   => 755, 
+      ensure => directory;
+    "/etc/mysql-proxy/lua.d":
+      owner   => root,
+      group   => root,
+      mode    => 0644,
+      recurse => true,
+      purge   => true,
+      source  => "puppet:///modules/mysqlproxy/lua.d",
+      ensure  => directory;
+  }
+  if ($luascript !='')
+  {
+    file{
+      "/etc/mysql-proxy/lua.d/${luascript}":
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        source  => "puppet:///modules/mysqlproxy/lua.d/${luascript}";
+    }
+  }
 }
